9c61ca94fa8858a8a78bce38255a4c42196ba050
Leanne Holmes <lholmes24@my.bcit.ca>
2023-02-05 14:00:34 -0800

    Positioned top and left divs

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Header from './Components/Header';
 const App = () => {
   return (
     <div className='container'>
-      <Sidebar />
       <Header />
+      <Sidebar />
       <Content />
     </div>
   );
