19d14bdd521c65e5fbeb21fc6815c6b81e39e85c
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-02-05 17:22:51 -0800

    Add MUI button on the header with accept only .tab and .csv files.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '@mui/material/Button'
 import '../css/Header.css'
 
 const Header = () => {
     return (
         <div className='header'>
-            file upload here
+            <Button variant="contained" component="label">
+            Upload
+            <input hidden accept=".tab, .csv" multiple type="file" />
+            </Button>
         </div>
     );
 };
