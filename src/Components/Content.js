c6f4a31f4efac3261dad90161518aa01feba46a8
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-02-07 14:41:02 -0800

    Impelement html to svg image and pdf file.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,5 +1,13 @@
+//import html2canvas from "html2canvas";
 import Timeline from "./Timeline";
 import Venue from "./Venue";
+import jsPDF from "jspdf";
+import Button from '@mui/material/Button'
+import * as htmlToImage from 'html-to-image';
+import download from "downloadjs";
+
+//import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+
 
 const Content = (props) => {
     const scheduleDetail = [
@@ -270,7 +278,9 @@ const Content = (props) => {
 
     ]
     return (
-        <div className="content">
+        <div id="content">
+
+            <Button variant="contained" component="label" onClick={converToPdf}>TEST</Button>
             <h3>Dashboard</h3>
             {scheduleDetail.map((element, index) => {
                 return (
@@ -283,6 +293,53 @@ const Content = (props) => {
             })}
         </div>
     );
+};
+
+
+const converToPdf = async () => {
+
+    //1.html을 들고와서 canvas화
+    //const canvas = await html2canvas(document.getElementById('content'));
+    const canvas = await htmlToImage.toSvg(document.getElementById('content'));
+
+    htmlToImage.toSvg(document.getElementById('content'))
+    .then(function (dataUrl) {
+        download(dataUrl, 'my-node.svg');
+    });
+
+    //2.이미지화
+    // const imageFile = canvas.toDataURL('image/svg');
+    //3.pdf준비
+    const doc = new jsPDF('l', 'px', [3067, 2713]);
+    //pdf 가로 세로 사이즈
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+
+    //이미지의 길이와 pdf의 가로길이가 다르므로 이미지 길이를 기준으로 비율을 구함
+    const widthRatio = pageWidth / canvas.width;
+    //비율에 따른 이미지 높이
+    const customHeight = canvas.height * widthRatio;
+    //pdf에 1장에 대한 이미지 추가
+    doc.addImage(canvas, 'svg', 0, 0, pageWidth, customHeight);
+    //doc.addImage(imgData, 'PNG', margin, position, imgWidth, imgHeight);
+    //감소하면서 남은 길이 변수
+    let heightLeft = customHeight;
+    //증가하면서 이미지 자를 위치 변수
+    let heightAdd = -pageHeight;
+
+    // 한 페이지 이상일 경우
+    while (heightLeft >= pageHeight) {
+        //pdf페이지 추가
+        doc.addPage();
+        //남은 이미지를 추가
+        doc.addImage(canvas, 'svg', 0, heightAdd, pageWidth, customHeight);
+        //남은길이
+        heightLeft -= pageHeight;
+        //남은높이
+        heightAdd -= pageHeight;
+    }
+    //문서저장
+    doc.save('filename' + new Date().getTime() + '.pdf');
 };
 
 export default Content
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../App'
 import Button from '@mui/material/Button'
 import '../css/Header.css'
 
 const Header = () => {
     return (
         <div className='header'>
-            <div className='header1'>            <Button variant="contained" component="label">
-            Upload
-            <input hidden accept=".tab, .csv" multiple type="file" />
-            </Button></div>
+            <div className='header1'>
+                <Button variant="contained" component="label">
+                    Upload
+                    <input hidden accept=".tab, .csv" multiple type="file" />
+                </Button>
+            </div>
             <div className='header2'></div>
             <div className='header3'></div>
             <div className='header4'></div>
