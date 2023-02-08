1aa611ad5ce3c7bb3c706b04b7de1b770b889057
Leanne Holmes <lholmes24@my.bcit.ca>
2023-02-07 22:00:50 -0800

    Corrected schedule css

--- src/Components/Timeline.js
+++ src/Components/Timeline.js
@@ -6,21 +6,22 @@ const Timeline = (prop) => {
         <div className='timelineRow'>
             <div className='timelineDate'>{prop.date}</div>
             <div className='timeline'>
-                <div className='time1'>9:00AM</div>
-                <div className='time2'>10:00AM</div>
-                <div className='time3'>11:00AM</div>
-                <div className='time4'>12:00PM</div>
-                <div className='time5'>1:00PM</div>
-                <div className='time6'>2:00PM</div>
-                <div className='time7'>3:00PM</div>
-                <div className='time8'>4:00PM</div>
-                <div className='time9'>5:00PM</div>
-                <div className='time10'>6:00PM</div>
-                <div className='time11'>7:00PM</div>
-                <div className='time12'>8:00PM</div>
-                <div className='time13'>9:00PM</div>
-                <div className='time14'>10:00PM</div>
-                <div className='time15'>11:00PM</div>
+                <div className='time'>9:00AM</div>
+                <div className='time'>10:00AM</div>
+                <div className='time'>11:00AM</div>
+                <div className='time'>12:00PM</div>
+                <div className='time'>1:00PM</div>
+                <div className='time'>2:00PM</div>
+                <div className='time'>3:00PM</div>
+                <div className='time'>4:00PM</div>
+                <div className='time'>5:00PM</div>
+                <div className='time'>6:00PM</div>
+                <div className='time'>7:00PM</div>
+                <div className='time'>8:00PM</div>
+                <div className='time'>9:00PM</div>
+                <div className='time'>10:00PM</div>
+                <div className='time'>11:00PM</div>
+                <div className='time'>12:00AM</div>
             </div>
         </div>
     );
--- src/Components/Venue.js
+++ src/Components/Venue.js
@@ -6,78 +6,91 @@ const Venue = (prop) => {
         <div className='venueRow'>
             <div className='venue'>{prop.venueDetail.venueName}</div>
             <div className='screen'>
-                {prop.venueDetail.screens.map((element, index) => {
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+
+            <div className='grid1'></div>
+            <div className='grid2'></div>
+            <div className='grid1'></div>
+            <div className='grid1'></div>
+                {/* {prop.venueDetail.screens.map((element, index) => {
                 
                     return (
-                        <>
-                        <div className='screen1'></div>
-                        <div className='screen2'></div>
-                        <div className='screen3'></div>
-                        <div className='screen4'></div>
-                        <div className='screen5'></div>
-                        <div className='screen6'></div>
-                        <div className='screen7'></div>
-                        <div className='screen8'></div>
-                        <div className='screen9'></div>
-                        <div className='screen10'></div>
-                        <div className='screen11'></div>
-                        <div className='screen12'></div>
-                        <div className='screen13'></div>
-                        <div className='screen14'></div>
-                        <div className='screen15'></div>
-                        <div className='screen16'></div>
-                        <div className='screen17'></div>
-                        <div className='screen18'></div>
-                        <div className='screen19'></div>
-                        <div className='screen20'></div>
-                        <div className='screen21'></div>
-                        <div className='screen22'></div>
-                        <div className='screen23'></div>
-                        <div className='screen24'></div>
-                        <div className='screen25'></div>
-                        <div className='screen26'></div>
-                        <div className='screen27'></div>
-                        <div className='screen28'></div>
-                        <div className='screen29'></div>
-                        <div className='screen30'></div>
-                        <div className='screen31'></div>
-                        <div className='screen32'></div>
-                        <div className='screen33'></div>
-                        <div className='screen34'></div>
-                        <div className='screen35'></div>
-                        <div className='screen36'></div>
-                        <div className='screen37'></div>
-                        <div className='screen38'></div>
-                        <div className='screen39'></div>
-                        <div className='screen40'></div>
-                        <div className='screen41'></div>
-                        <div className='screen42'></div>
-                        <div className='screen43'></div>
-                        <div className='screen44'></div>
-                        <div className='screen45'></div>
-                        <div className='screen46'></div>
-                        <div className='screen47'></div>
-                        <div className='screen40'></div>
-                        <div className='screen41'></div>
-                        <div className='screen42'></div>
-                        <div className='screen43'></div>
-                        <div className='screen48'></div>
-                        <div className='screen49'></div>
-                        <div className='screen50'></div>
-                        <div className='screen51'></div>
-                        <div className='screen52'></div>
-                        <div className='screen53'></div>
-                        <div className='screen54'></div>
-                        <div className='screen55'></div>
-                        <div className='screen56'></div>
-                        <div className='screen57'></div>
-                        <div className='screen58'></div>
-                        <div className='screen59'></div>
-                        <div className='screen60'></div>
-
-                        </>
+                        <div></div>
                     )
-                })}
+                })} */}
             </div>
         </div>
     );
