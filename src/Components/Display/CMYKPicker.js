11006a3dd3c5d4fba545ce31edabbad8b8486763
Leanne Holmes <lholmes24@my.bcit.ca>
2023-02-28 09:07:08 -0800

    Added a CMYK color picker class

---
+++ src/Components/Display/CMYKPicker.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ColorPicker } from 'react-colors';
+
+class CMYKPicker extends React.Component {
+
+  handleChange(color, e) {
+      /* color = {
+            rgb: [80, 134, 144],
+            rgba: [80, 134, 144, 0.572],
+            hsl: [189.38, 0.286, 0.439],
+            hsv: [189.38, 0.444, 0.565],
+            lab: [52.69, -15.13, -10.98],
+            lch: [52.69, 18.69, 215.96],
+            hcl: [215.95, 18.69, 52.69],
+            cmyk: [0.444, 0.0694, 0, 0.435],
+            css: "rgba(80,134,144,0.5723760738357511)",
+            hex: "#508690",
+            temperature: 40000,
+            a: 0.572,
+            alpha: 0.572,
+        } */
+  }
+//   render() {
+//     return (
+//         <ColorPicker onChange={ this.handleChange }>{ // some children }</ColorPicker>
+//   }
+}
+
+export default CMYKPicker;
