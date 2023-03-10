7d4a4975e29920ca8d89468ed7d6b3712aadfade
RichardOhata <richardohata@gmail.com>
2023-03-10 01:31:53 -0800

    Added functionality to CMYK Converter

--- src/Components/Display/Sidebar.js
+++ src/Components/Display/Sidebar.js
@@ -8,7 +8,7 @@ import DateBarPicker from './CustomPickers/DateBarPicker';
 import '../../css/Sidebar.css';
 
 // External import
-import React from 'react';
+import React, { useState } from 'react';
 import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
@@ -27,13 +27,28 @@ import MaterialIcon, {colorPalette} from 'material-icons-react';
 import OutlinedInput from '@mui/material/OutlinedInput';  
 import FormHelperText from '@mui/material/FormHelperText';
 import TextField from '@mui/material/TextField';
-import { text } from '@fortawesome/fontawesome-svg-core';
-import { CenterFocusStrong } from '@mui/icons-material';
-
-
 
 
 const Sidebar = () => {
+  const [cValue, setCValue] = useState('');
+  const [mValue, setMValue] = useState('');
+  const [yValue, setYValue] = useState('');
+  const [kValue, setKValue] = useState('');
+  const [hexValue, setHexValue] = useState(' ');
+  const [rValue, setRValue] = useState('');
+  const [gValue, setGValue] = useState('');
+  const [bValue, setBValue] = useState('');
+  const convert = require('color-convert');
+
+  const convertToHex= (e) => {
+    if(cValue != '' && mValue != '' && yValue != '' && kValue != '') {
+      setHexValue(convert.cmyk.hex(Number(cValue), Number(mValue), Number(yValue), Number(kValue)));
+      let rgbArray = convert.cmyk.rgb(Number(cValue), Number(mValue), Number(yValue), Number(kValue));
+      setRValue(rgbArray[0]);
+      setGValue(rgbArray[1]);
+      setBValue(rgbArray[2]);
+    }
+  }
     return (
         <div className='sidebar'>
             <h3>Customization Menu</h3>
@@ -56,6 +71,8 @@ const Sidebar = () => {
             size="small"
             autoComplete='off'
             aria-describedby="outlined-weight-helper-text"
+            value={cValue}
+            onChange={(e) => setCValue(e.target.value)}
             inputProps={{
               'aria-label': 'weight',
               inputMode: 'numeric',
@@ -71,6 +88,8 @@ const Sidebar = () => {
             size="small"
             autoComplete='off'
             aria-describedby="outlined-weight-helper-text"
+            value={mValue}
+            onChange={(e) => setMValue(e.target.value)}
             inputProps={{
               'aria-label': 'weight',
               inputMode: 'numeric',
@@ -86,6 +105,8 @@ const Sidebar = () => {
             size="small"
             autoComplete='off'
             aria-describedby="outlined-weight-helper-text"
+            value={yValue}
+            onChange={(e) => setYValue(e.target.value)}
             inputProps={{
               'aria-label': 'weight',
               inputMode: 'numeric',
@@ -101,6 +122,8 @@ const Sidebar = () => {
             size="small"
             autoComplete='off'
             aria-describedby="outlined-weight-helper-text"
+            value={kValue}
+            onChange={(e) => setKValue(e.target.value)}
             inputProps={{
               'aria-label': 'weight',
               inputMode: 'numeric',
@@ -114,7 +137,7 @@ const Sidebar = () => {
       <div>
       <TextField  sx={{width: '100%', paddingBottom:3}}
       label="Hex"
-      defaultValue=" "
+      value={hexValue}
         id="outlined-basic" 
         variant="outlined"
         size="small"
@@ -129,9 +152,10 @@ const Sidebar = () => {
             id="outlined-adornment-weight"
             size="small"
             aria-describedby="outlined-weight-helper-text"
+            value={rValue}
             inputProps={{
               'aria-label': 'weight',
-              inputMode: 'numeric',
+              inputMode: 'text',
               pattern: '[0-9]*',
               maxLength: 3,
               readOnly: true
@@ -144,9 +168,10 @@ const Sidebar = () => {
             id="outlined-adornment-weight"
             size="small"
             aria-describedby="outlined-weight-helper-text"
+            value={gValue}
             inputProps={{
               'aria-label': 'weight',
-              inputMode: 'numeric',
+              inputMode: 'text',
               pattern: '[0-9]*',
               maxLength: 3,
               readOnly: true
@@ -159,9 +184,10 @@ const Sidebar = () => {
             id="outlined-adornment-weight"
             size="small"
             aria-describedby="outlined-weight-helper-text"
+            value={bValue}
             inputProps={{
               'aria-label': 'weight',
-              inputMode: 'numeric',
+              inputMode: 'text',
               pattern: '[0-9]*',
               maxLength: 3,
               readOnly: true
@@ -170,6 +196,9 @@ const Sidebar = () => {
           <FormHelperText id="outlined-weight-helper-text">B</FormHelperText>
         </FormControl>
       </div>
+      <button onClick={convertToHex}>
+        Convert
+      </button>
         </AccordionDetails>
       </Accordion>
       </div>
