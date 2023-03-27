35b8a7acdb689ec0e61b6d8aa8d70777f6c32579
GyoungHwan-Hong <hongkh5218@gmail.com>
2023-03-27 14:55:40 -0700

    Adds a function to store color settings user last picked by using cookie.

--- src/Context/ScheduleContext/ScheduleState.js
+++ src/Context/ScheduleContext/ScheduleState.js
@@ -3,7 +3,7 @@ import colorSettings from "../../resources/colors.json";
 import reactCSS from 'reactcss';
 import ScheduleContext from './ScheduleContext';
 import ScheduleReducer from './ScheduleReducer';
-import RNFetchBlob from 'react-native-fetch-blob';
+import { useCookies } from 'react-cookie';
 
 
 import {
@@ -14,25 +14,37 @@ import {
 } from '../ActionType';
 import { parseGridScreensHelper, mapVenueNameHelper } from '../../Helper/ParsingHelper';
 
-const ScheduleState = (props) => {
 
-    console.log(colorSettings);
+const ScheduleState = (props) => {
+    const [cookies, setCookie, removeCookie] = useCookies(['dateText','dateBar', 'timeText', 'venueText', 'venueBar', 'filmTitleText', 'filmDetailsText', 'filmBlock', 'oddRow', 'evenRow']);
+
+    const expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 1);
+
+
+    colorSettings.dateText = cookies.dateText !== undefined ? cookies.dateText : colorSettings.dateText;
+    colorSettings.dateBar = cookies.dateBar !== undefined ? cookies.dateBar : colorSettings.dateBar;
+    colorSettings.timeText = cookies.timeText !== undefined ? cookies.timeText : colorSettings.timeText;
+    colorSettings.venueText = cookies.venueText !== undefined ? cookies.venueText : colorSettings.venueText;
+    colorSettings.venueBar = cookies.venueBar !== undefined ? cookies.venueBar : colorSettings.venueBar;
+    colorSettings.filmTitleText = cookies.filmTitleText !== undefined ? cookies.filmTitleText : colorSettings.filmTitleText;
+    colorSettings.filmDetailsText = cookies.filmDetailsText !== undefined ? cookies.filmDetailsText : colorSettings.filmDetailsText;
+    colorSettings.filmBlock = cookies.filmBlock !== undefined ? cookies.filmBlock : colorSettings.filmBlock;
+    colorSettings.oddRow = cookies.oddRow !== undefined ? cookies.oddRow : colorSettings.oddRow;
+    colorSettings.evenRow = cookies.evenRow !== undefined ? cookies.evenRow : colorSettings.evenRow;
+
+
+    // if ( cookies.dateBar === null ) {
+    //      colorSettings = colorSettings.dateBar;
+    //  } else {
+    //      colorSettings.dateBar = cookies.dateBar;
+    //  }
+
+    
 
     const initialState = {
         gridScreenTimes: [],
         colorSettings,
-        // colorSettings: {
-        //     dateText: {r: 255, g: 165, b: 0, a: 1},
-        //     dateBar: {r: 0, g: 0, b: 0, a: 1},
-        //     timeText: {r: 255, g: 255, b: 255, a: 1},
-        //     venueText: {r: 0, g: 0, b: 255, a: 1},
-        //     venueBar: {r: 167, g: 169, b: 172, a: 1},
-        //     filmTitleText: {r: 0, g: 0, b: 0, a: 1},
-        //     filmDetailsText: {r: 0, g: 0, b: 0, a: 1},
-        //     filmBlock: {r: 255, g: 255, b: 255, a: 1},
-        //     oddRow: {r: 209, g: 202, b: 192, a: 1},
-        //     evenRow: {r: 158, g: 153, b: 145, a: 1}
-        // },
     };
 
     const [state, dispatch] = useReducer(ScheduleReducer, initialState);
@@ -69,6 +81,8 @@ const ScheduleState = (props) => {
             id: color.id,
             color: colorObject,
         });
+        setCookie(color.id, colorObject, { expires: expirationDate });
+
         // updateColorSettings(color.id, colorObject);
 
     };
