52ec2e7b2d1bf4a1d9fb4686099addc804e486ae
RichardOhata <richardohata@gmail.com>
2023-04-08 15:43:11 -0700

    Adjusted date text font size

--- src/Components/Display/SidebarComponents/FontSettings.js
+++ src/Components/Display/SidebarComponents/FontSettings.js
@@ -55,6 +55,8 @@ const FontSettings = () => {
                         <MenuItem value={6}>6</MenuItem>
                         <MenuItem value={7}>7</MenuItem>
                         <MenuItem value={8}>8</MenuItem>
+                        <MenuItem value={9}>9</MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
                     </Select></>
                 </FormControl>
             </div>
