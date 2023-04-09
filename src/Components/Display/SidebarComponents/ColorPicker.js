d421361794b35ebc29c19b3e290e8bb768342ebd
Man Sun <msun45@my.bcit.ca>
2023-04-09 16:33:42 -0700

    rewrite colorpicker to add useeffect

--- src/Components/Display/SidebarComponents/ColorPicker.js
+++ src/Components/Display/SidebarComponents/ColorPicker.js
@@ -1,86 +1,83 @@
-/* eslint-disable require-jsdoc */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import reactCSS from 'reactcss';
 import { SketchPicker } from 'react-color';
 
-class SketchExample extends React.Component {
-    state = {
-        displayColorPicker: false,
-        color: {
-            r: '241',
-            g: '112',
-            b: '19',
-            a: '1',
-        },
-    };
-    constructor(props) {
-        super(props);
-        this.state.color.r = this.props.color.r;
-        this.state.color.g = this.props.color.g;
-        this.state.color.b = this.props.color.b;
-        this.state.color.a = this.props.color.a;
-    }
+const SketchExample = (props) => {
+    const [displayColorPicker, setDisplayColorPicker] = useState(false);
+    const [color, setColor] = useState({
+        r: props.color.r,
+        g: props.color.g,
+        b: props.color.b,
+        a: props.color.a,
+    });
 
-    handleClick = () => {
-        this.setState({ displayColorPicker: !this.state.displayColorPicker });
-    };
-
-    handleClose = () => {
-        this.setState({ displayColorPicker: false });
-    };
-
-    handleChange = (color) => {
-        this.setState({ color: color.rgb });
-        this.props.parentCallBack(color.rgb, this.props.id);
-    };
-
-    render() {
-        const styles = reactCSS({
-            'default': {
-                color: {
-                    width: '36px',
-                    height: '14px',
-                    borderRadius: '2px',
-                    background: `rgba(${this.state.color.r}, 
-                        ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
-                },
-                swatch: {
-                    padding: '4px',
-                    background: '#fff',
-                    borderRadius: '1px',
-                    boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-                    display: 'inline-block',
-                    cursor: 'pointer',
-                },
-                popover: {
-                    position: 'fixed',
-                    zIndex: '2',
-                    bottom: '40vh',
-                    left: '180px',
-                },
-                cover: {
-                    position: 'fixed',
-                    top: '0px',
-                    right: '0px',
-                    bottom: '0px',
-                    left: '0px',
-                },
-            },
+    useEffect(() => {
+        setColor({
+            r: props.color.r,
+            g: props.color.g,
+            b: props.color.b,
+            a: props.color.a,
         });
+    }, [props.color]);
 
-        return (
-            <div>
-                <div style={styles.swatch} onClick={this.handleClick}>
-                    <div style={styles.color} />
-                </div>
-                {this.state.displayColorPicker ? <div style={styles.popover}>
-                    <div style={styles.cover} onClick={this.handleClose} />
-                    <SketchPicker color={this.state.color} onChange={this.handleChange} />
-                </div> : null}
+    const handleClick = () => {
+        setDisplayColorPicker(!displayColorPicker);
+    };
 
+    const handleClose = () => {
+        setDisplayColorPicker(false);
+    };
+
+    const handleChange = (color) => {
+        setColor(color.rgb);
+        props.parentCallBack(color.rgb, props.id);
+    };
+
+    const styles = reactCSS({
+        'default': {
+            color: {
+                width: '36px',
+                height: '14px',
+                borderRadius: '2px',
+                background: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
+            },
+            swatch: {
+                padding: '4px',
+                background: '#fff',
+                borderRadius: '1px',
+                boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+                display: 'inline-block',
+                cursor: 'pointer',
+            },
+            popover: {
+                position: 'fixed',
+                zIndex: '2',
+                bottom: '40vh',
+                left: '180px',
+            },
+            cover: {
+                position: 'fixed',
+                top: '0px',
+                right: '0px',
+                bottom: '0px',
+                left: '0px',
+            },
+        },
+    });
+
+    return (
+        <div>
+            <div style={styles.swatch} onClick={handleClick}>
+                <div style={styles.color} />
             </div>
-        );
-    }
-}
+            {displayColorPicker ? (
+                <div style={styles.popover}>
+                    <div style={styles.cover} onClick={handleClose} />
+                    <SketchPicker color={color} onChange={handleChange} />
+                </div>
+            ) : null}
+        </div>
+    );
+};
 
 export default SketchExample;
--- src/Components/Display/SidebarComponents/ColorSettings.js
+++ src/Components/Display/SidebarComponents/ColorSettings.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from 'react';
+import { useState, useEffect } from 'react';
 import defaultSettings from '../../../resources/colors_default.json';
 import ColorPicker from './ColorPicker';
 import { useScheduleContext } from '../../../Context/ScheduleContext/ScheduleContext';
@@ -15,28 +16,32 @@ import Button from '@mui/material/Button';
 
 
 const ColorSettings = () => {
-
     const colordatafromjson = defaultSettings;
     const scheduleContext = useScheduleContext();
-    const colors = scheduleContext.colorSettings;
+    // const colors = scheduleContext.colorSettings;
+    const [colors, setColors] = useState(scheduleContext.colorSettings);
+    useEffect(() => {
+        setColors(scheduleContext.colorSettings);
+    }, [scheduleContext.colorSettings]);
+
     const handleCallBack = (childData, id) => {
         const colorData = { r: childData.r, g: childData.g, b: childData.b, a: childData.a, id: id };
         scheduleContext.setColor(colorData);
     };
 
     const handleClick = () => {
-            console.log("hello");
-            handleCallBack(colordatafromjson.dateBar,'dateBar');
-            handleCallBack(colordatafromjson.dateText,'dateText');
-            handleCallBack(colordatafromjson.timeText,'timeText');
-            handleCallBack(colordatafromjson.venueText,'venueText');
-            handleCallBack(colordatafromjson.venueBar,'venueBar');
-            handleCallBack(colordatafromjson.filmTitleText,'filmTitleText');
-            handleCallBack(colordatafromjson.filmDetailsText,'filmDetailsText');
-            handleCallBack(colordatafromjson.filmBlock,'filmBlock');
-            handleCallBack(colordatafromjson.oddRow,'oddRow');
-            handleCallBack(colordatafromjson.evenRow,'evenRow');
-      };
+        console.log('hello');
+        handleCallBack(colordatafromjson.dateBar, 'dateBar');
+        handleCallBack(colordatafromjson.dateText, 'dateText');
+        handleCallBack(colordatafromjson.timeText, 'timeText');
+        handleCallBack(colordatafromjson.venueText, 'venueText');
+        handleCallBack(colordatafromjson.venueBar, 'venueBar');
+        handleCallBack(colordatafromjson.filmTitleText, 'filmTitleText');
+        handleCallBack(colordatafromjson.filmDetailsText, 'filmDetailsText');
+        handleCallBack(colordatafromjson.filmBlock, 'filmBlock');
+        handleCallBack(colordatafromjson.oddRow, 'oddRow');
+        handleCallBack(colordatafromjson.evenRow, 'evenRow');
+    };
 
     return (
         <div className="accordionItems">
@@ -60,8 +65,8 @@ const ColorSettings = () => {
             <div id='colorPicker'><ColorPicker parentCallBack={handleCallBack} id={'oddRow'} color={colors['oddRow']} /></div>
             <p id="colorLabels">Even Row</p>
             <div id='colorPicker'><ColorPicker parentCallBack={handleCallBack} id={'evenRow'} color={colors['evenRow']} /></div>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Button onClick={handleClick} variant="outlined">Load default</Button>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <Button onClick={handleClick} variant="outlined">Load default</Button>
             </div>
         </div>
     );
